0460c6d406d2db8dfe3ab4ddc1063178acbc5b05
ns408 <nsrea408@gmail.com>
2020-07-06 17:42:54 +1000

    Update build_deps.cmd

--- build_deps.cmd
+++ build_deps.cmd
@@ -1,8 +1,11 @@
 @echo on
+choco install windows-sdk-8.1
+refreshenv
+
 set ProjectFolder=%CD%
 mkdir %CD%\win_depends
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
 :compile_curl
 cd win_depends 
 git clone https://github.com/curl/curl
@@ -10,6 +13,7 @@ cd curl
 call buildconf.bat
 cd winbuild
 rem set RTLIBCFG=static
+:WHERE /R "C:\Program Files (x86)\Microsoft Visual Studio\2019" "nmake"
 nmake /f Makefile.vc mode=static vc=16 debug=no ENABLE_WINSSL=yes
 nmake /f Makefile.vc mode=static vc=16 debug=yes ENABLE_WINSSL=yes
 :install 
