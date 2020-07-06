98e46b80a085295cc8040874a63aa8b879b531fa
ns408 <nsrea408@gmail.com>
2020-07-06 17:35:57 +1000

    Create build_dep2.cmd

---
+++ build_dep2.cmd
@@ -0,0 +1,20 @@
+@echo on
+choco install windows-sdk-8.1
+refreshenv
+set ProjectFolder=%CD%
+mkdir %CD%\win_depends
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
+echo %errorlevel%
+cd win_depends
+git clone https://github.com/curl/curl
+cd curl
+buildconf.bat
+cd winbuild
+rem set RTLIBCFG=static
+nmake /f Makefile.vc mode=static vc=16 debug=no ENABLE_WINSSL=yes
+nmake /f Makefile.vc mode=static vc=16 debug=yes ENABLE_WINSSL=yes
+:install 
+echo a |xcopy /e /h /c /i "../builds/libcurl-vc16-x64-release-static-ipv6-sspi-winssl/include" "%ProjectFolder%\includes"
+echo %errorlevel%
+msbuild dapps_win.vcxproj
+msbuild subatomic.vcxproj
